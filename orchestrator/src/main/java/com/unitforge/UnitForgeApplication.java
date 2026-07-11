2b126baf94cedfe756689259c89fa7ed68c60b6b
Akshat-Tated <akshattated18@gmail.com>
2026-07-11 22:45:04 +0530

    chore(orchestrator): initialize Spring Boot project

---
+++ orchestrator/src/main/java/com/unitforge/UnitForgeApplication.java
@@ -0,0 +1,12 @@
+package com.unitforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitForgeApplication.class, args);
+    }
+}
